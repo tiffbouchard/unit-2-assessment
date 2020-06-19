c8c704f538539d6ef5f2d90c3b02211fc9b0ea2f
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-19 15:51:28 -0400

    function to post todo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Todo = require("../data/todo");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Unit 2 Assessment",
+    todos: Todo,
+  });
+});
+
+router.post("/add", function (req, res, next) {
+  req.body.todo = req.body.todo;
+  req.body.id = Date.now() % 1000000;
+  req.body.done = false;
+  // req.body.done = false;
+  Todo.push(req.body);
+  console.log(Todo);
+  res.redirect("/");
 });
 
 module.exports = router;
+
+// function create(todo) {
+//   // Add the id
+//   todo.id = Date.now() % 1000000;
+//   // New todos wouldn't be done :)
+//   todo.done = false;
+//   todos.push(todo);
+// }
+
+// function create(req, res) {
+//   console.log(req.body);
+//   req.body.done = false;
+//   Todo.create(req.body);
+//   res.redirect("/todos");
+// }
